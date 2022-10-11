212973d108788f5754e519f25b44761aa4ab8f3d
Marius Mikučionis <marius@cs.aau.dk>
2022-10-11 17:13:52 +0200

    Restructured files and scripts across directories

--- cmake-linux.sh
+++ cmake-linux.sh
@@ -5,23 +5,23 @@ set -e
 CORES=$(nproc)
 
 # Debug build, useful when debugging the library:
-BUILD_DIR=cmake-linux64-debug
-cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Debug
+BUILD_DIR=build-linux-debug
+cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Debug -DUBSAN=ON -DASAN=ON
 cmake --build "$BUILD_DIR" -j$CORES
 ## Test the build:
 (cd "$BUILD_DIR" ; ctest --output-on-failure -j$CORES)
 ## Create a link to it:
 if [ ! -e libtable-dbg.so ]; then
-	ln -s "$BUILD_DIR"/libtable.so libtable-dbg.so
+	ln -s "$BUILD_DIR"/src/libtable.so libtable-dbg.so
 fi
 
 # Optimized release build:
-BUILD_DIR=cmake-linux64-release
+BUILD_DIR=build-linux-release
 cmake -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release
 cmake --build "$BUILD_DIR" -j$CORES
 ## Test the build:
 (cd "$BUILD_DIR" ; ctest --output-on-failure -j$CORES)
 ## Create a link to it:
 if [ ! -e libtable.so ]; then
-	ln -s "$BUILD_DIR"/libtable.so libtable.so
+	ln -s "$BUILD_DIR"/src/libtable.so libtable.so
 fi
