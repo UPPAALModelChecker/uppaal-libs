3c95585e43372ed96539b9c6c8d3e216af3efc68
Marius Mikučionis <marius@cs.aau.dk>
2023-06-12 09:58:41 +0200

    replaced gettimofday with C++ chrono to make it portable (for Windows)

--- src/errors.cpp
+++ src/errors.cpp
@@ -4,21 +4,20 @@
  */
 
 #include <string>
-#include <cassert>
+#include <chrono>
 #include <cstdio>	   // fopen, fprintf
 #include <cstdarg>	   // va_list
-#include <unistd.h>	   // getcwd
-#include <sys/time.h>  // gettimeofday
 
 void log_error(const char* function, const char* path, int line, const char* format, ...)
 {
 	static FILE* file = std::fopen("error.log", "w");
-	timeval tv;
-	gettimeofday(&tv, nullptr);
+	auto time = std::chrono::system_clock::now().time_since_epoch();
+	auto sec = std::chrono::duration_cast<std::chrono::seconds>(time);
+	auto usec = std::chrono::duration_cast<std::chrono::microseconds>(time-sec);
 #if defined(__APPLE__) && defined(__MACH__)
-	fprintf(file, "%ld.%06d ", tv.tv_sec, tv.tv_usec);
+	fprintf(file, "%ld.%06d ", sec.count(), usec.count());
 #else
-	fprintf(file, "%d.%06d ", tv.tv_sec, tv.tv_usec);
+	fprintf(file, "%d.%06d ", sec.count(), usec.count());
 #endif
 	va_list argp;
 	va_start(argp, format);
