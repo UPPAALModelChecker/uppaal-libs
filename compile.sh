1a9505b0ba6ab9b8bfc06c1671a5fbc742cbd13b
Marius Mikučionis <marius@cs.aau.dk>
2026-03-16 16:14:30 +0100

    Added more compilers

--- compile.sh
+++ compile.sh
@@ -20,7 +20,7 @@ targets="$@"
 
 if [ -z "$targets" ]; then
   echo "Expected a target platform as an argument. The following are supported:"
-  for target in $(ls toolchain) ; do
+  for target in $(ls cmake/toolchain) ; do
     t=${target%%.cmake}
     echo -e "\t$t"
   done
@@ -30,28 +30,18 @@ if [ -z "$targets" ]; then
     targets=""
     if [ -n "$(command -v c++)" ]; then
       targets="$targets linux64"
-      if [ -n "$(c++ -m32 --print-file-name=libstdc++.so)" ]; then
+      if echo 'int main(){}' | c++ -m32 -xc++ - -o /dev/null 2>&1 ; then
         targets="$targets linux32"
       fi
     fi
-    if [ -n "$(command -v g++-10)" ]; then
-      targets="$targets linux64-gcc10"
-      if [ -n "$(g++-10 -m32 --print-file-name=libstdc++.so)" ]; then
-        targets="$targets linux32-gcc10"
+    for COMPILER in g++-16 g++-15 g++-14 g++-13 g++-12 g++-11 g++-10 ; do
+      if [ -n "$(command -v "$COMPILER")" ]; then
+        targets="$targets linux64-${COMPILER/++-/cc}"
+        if echo 'int main(){}' | ${COMPILER} -m32 -xc++ - -o /dev/null 2>&1 ; then
+          targets="$targets linux32-${COMPILER/++-/cc}"
+        fi
       fi
-    fi
-    if [ -n "$(command -v g++-11)" ]; then
-      targets="$targets linux64-gcc11"
-      if [ -n "$(g++-11 -m32 --print-file-name=libstdc++.so)" ]; then
-        targets="$targets linux32-gcc11"
-      fi
-    fi
-    if [ -n "$(command -v g++-12)" ]; then
-      targets="$targets linux64-gcc12"
-      if [ -n "$(g++-12 -m32 --print-file-name=libstdc++.so)" ]; then
-        targets="$targets linux32-gcc12"
-      fi
-    fi
+    done
     if [ -n "$(command -v x86_64-w64-mingw32-g++)" ]; then
       targets="$targets x86_64-w64-mingw32"
     fi
@@ -64,24 +54,16 @@ if [ -z "$targets" ]; then
     if [ -n "$(command -v c++)" ]; then
       targets="$targets macos64"
     fi
-    if [ -n "$(command -v g++-10)" ]; then
-      targets="$targets macos64-brew-gcc10"
-    fi
-    if [ -n "$(command -v g++-11)" ]; then
-      targets="$targets macos64-brew-gcc11"
-    fi
-    if [ -n "$(command -v g++-12)" ]; then
-      targets="$targets macos64-brew-gcc12"
-    fi
-    if [ -n "$(command -v g++-mp-10)" ]; then
-      targets="$targets macos64-ports-gcc10"
-    fi
-    if [ -n "$(command -v g++-mp-11)" ]; then
-      targets="$targets macos64-ports-gcc11"
-    fi
-    if [ -n "$(command -v g++-mp-12)" ]; then
-      targets="$targets macos64-ports-gcc12"
-    fi
+    for COMPILER in g++-16 g++-15 g++-14 g++-13 g++-12 g++-11 g++-10 ; do
+      if [ -n "$(command -v "$COMPILER")" ]; then
+        targets="$targets macos64-brew-${COMPILER}"
+      fi
+    done
+    for COMPILER in g++-mp-16 g++-mp-15 g++-mp-14 g++-mp-13 g++-mp-12 g++-mp-11 g++-mp-10 ; do
+      if [ -n "$(command -v "$COMPILER")" ]; then
+        targets="$targets macos64-ports-${COMPILER}"
+      fi
+    done
     ;;
   *)
     echo "Unknown hosting platform"
@@ -106,15 +88,18 @@ fi
 if [ -z "$CTEST_TEST_LOAD" ]; then
   export CTEST_TEST_LOAD=$CORES
 fi
+if [ -z "$CTEST_OUTPUT_ON_FAILURE" ]; then
+  export CTEST_OUTPUT_ON_FAILURE=1
+fi
 
 for target in $targets ; do
   unset CMAKE_TOOLCHAIN_FILE
   unset SANITIZE
-  if [ ! -r $PWD/toolchain/${target}.cmake ]; then
-    echo "The toolchain file does not exist: $PWD/toolchain/${target}.cmake"
+  if [ ! -r $PWD/cmake/toolchain/${target}.cmake ]; then
+    echo "The toolchain file does not exist: $PWD/cmake/toolchain/${target}.cmake"
     exit 1
   else
-    export CMAKE_TOOLCHAIN_FILE="$PWD/toolchain/${target}.cmake"
+    export CMAKE_TOOLCHAIN_FILE="$PWD/cmake/toolchain/${target}.cmake"
   fi
   case $target in
   linux*)
@@ -132,17 +117,36 @@ for target in $targets ; do
   i686*mingw32)
     extension=dll
     SANITIZE="-DSSP=ON"
+    export WINEARCH=win32
+    export WINEPREFIX="${HOME}/.wine32"
+    if [ ! -d "$WINEPREFIX" ]; then wine cmd /c echo "Prepared WINEPREFIX"; fi
     libgcc_path=$($target-g++ --print-file-name=libgcc_s_dw2-1.dll)
     libgcc_path=$(realpath "$libgcc_path")
     libgcc_path=$(dirname "$libgcc_path")
     libwinpthread_path=$($target-g++ --print-file-name=libwinpthread-1.dll)
     libwinpthread_path=$(realpath "$libwinpthread_path")
     libwinpthread_path=$(dirname "$libwinpthread_path")
-    export WINEPATH="$libwinpthread_path;$libgcc_path"
+    export WINEPATH="${libwinpthread_path};${libgcc_path}"
+    echo "WINEARCH=$WINEARCH"
+    echo "WINEPATH=$WINEPATH"
+    echo "WINEPREFIX=$WINEPREFIX"
     ;;
   x86_64*mingw32)
     extension=dll
     SANITIZE="-DSSP=ON"
+    export WINEARCH=win64
+    export WINEPREFIX="${HOME}/.wine64"
+    if [ ! -d "$WINEPREFIX" ]; then wine cmd /c echo "Prepared WINEPREFIX"; fi
+    libgcc_path=$($target-g++ --print-file-name=libgcc_s_seh-1.dll)
+    libgcc_path=$(realpath "$libgcc_path")
+    libgcc_path=$(dirname "$libgcc_path")
+    libwinpthread_path=$($target-g++ --print-file-name=libwinpthread-1.dll)
+    libwinpthread_path=$(realpath "$libwinpthread_path")
+    libwinpthread_path=$(dirname "$libwinpthread_path")
+    export WINEPATH="${libwinpthread_path};${libgcc_path}"
+    echo "WINEPREFIX=$WINEPREFIX"
+    echo "WINEPATH=$WINEPATH"
+    echo "WINEARCH=$WINEARCH"
     ;;
   *)
     echo "Unknown target platform: $target"
@@ -156,11 +160,9 @@ for target in $targets ; do
   echo "Building debug configuration for $target"
   cmake --build "$BUILD_DIR" --config $CMAKE_BUILD_TYPE
   echo "Testing debug configuration for $target"
-  (cd "$BUILD_DIR" ; ctest -C $CMAKE_BUILD_TYPE --output-on-failure)
+  ctest --test-dir "$BUILD_DIR" -C $CMAKE_BUILD_TYPE
   ## Create a link to it:
-  if [ ! -e libtable-dbg.${extension} ]; then
-	  ln -s "$BUILD_DIR"/src/libtable.${extension} libtable-dbg.${extension}
-  fi
+	ln -snf "${BUILD_DIR}/src/libtable.${extension}" "libtable-dbg.${extension}"
 
   export CMAKE_BUILD_TYPE=Release
   BUILD_DIR=build-$target-$CMAKE_BUILD_TYPE
@@ -169,9 +171,7 @@ for target in $targets ; do
   echo "Building optimized release configuration for $target"
   cmake --build "$BUILD_DIR" --config $CMAKE_BUILD_TYPE
   echo "Testing optimized release configuration for $target"
-  (cd "$BUILD_DIR" ; ctest -C $CMAKE_BUILD_TYPE --output-on-failure)
+  ctest --test-dir "$BUILD_DIR" -C $CMAKE_BUILD_TYPE
   ## Create a link to it:
-  if [ ! -e libtable.${extension} ]; then
-	  ln -s "$BUILD_DIR"/src/libtable.${extension} libtable.${extension}
-  fi
+	ln -snf "${BUILD_DIR}/src/libtable.${extension}" "libtable.${extension}"
 done
